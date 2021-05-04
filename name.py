1dfeb1b5c830f0574fa4687556007255e0844483
Ratkilu <lukasz.ratkiewicz@gmail.com>
2021-05-04 12:26:25 +0200

    some new change

--- name.py
+++ name.py
@@ -1,2 +1,7 @@
-text = input('What\'s your name?\n')
-print('Hello {}!'.format(text))
+name = input('What\'s your name?\n')
+print('Hello {}!'.format(name))
+age = input('How old are you?\n')
+if  >= 18):
+    print('You are {} years old, you can get a drivers license!'.format(age))
+else:
+    print('You are too young, to apply for a drivers license.')
